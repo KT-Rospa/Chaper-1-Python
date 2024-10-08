3eab9c821e0290f4b026c98b0625c1927dea8328
KT-Rospa <ctw20.kacper.trela@corketb.ie>
2024-10-08 21:33:03 +0100

    Add files via upload

---
+++ Task5pg31.py
@@ -0,0 +1,22 @@
+# Author : Kacper Trela
+# Date : Oct 2024
+# Task 5 pg 31
+
+mark = int(input("Enter your marks:"))
+
+if mark <= 29:
+    print("H8")
+elif mark <= 39:
+    print("H7")
+elif mark <= 49:
+    print("H6")
+elif mark <= 59:
+    print("H5")
+elif mark <= 69:
+    print("H4")
+elif mark <= 79:
+    print("H3")
+elif mark <= 89:
+    print("H2")
+elif mark <= 100:
+    print("H1")
